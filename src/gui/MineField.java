54ee65b78a798623d4cc6cbdb48ab2e0dd2136ea
Elliot <elliot.j@live.se>
2013-12-28 19:53:33 +0100

    dont know what im committing

--- src/gui/MineField.java
+++ src/gui/MineField.java
@@ -13,7 +13,7 @@ public class MineField {
 	public int[][] mines;
 	public int progress;
 
-	private final int height = 16, width = 30, nbrOfMines = 99;
+	private final int height = 10, width = 10, nbrOfMines = 10; // fix this!
 	private String[] options = { "Börja om", "Avsluta" };
 	private boolean isStarted = false;
 	private MineButton[][] matrix;
